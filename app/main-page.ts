0471706689752752e52d026f4eee0213929ae6b2
ccraddo <45082319+ccraddo@users.noreply.github.com>
2023-01-06 19:08:05 -0500

    4 files modified - refactor

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,8 +1,8 @@
 import { EventData, Page } from '@nativescript/core';
-import { HelloWorldModel } from './main-view-model';
+import { LocationModel } from './main-view-model';
 
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object;
 
-  page.bindingContext = new HelloWorldModel();
+  page.bindingContext = new LocationModel();
 }
--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -2,81 +2,138 @@ import { Observable } from '@nativescript/core';
 import * as geolocation from '@nativescript/geolocation';
 import { CoreTypes } from '@nativescript/core';
 
-export class HelloWorldModel extends Observable {
-  private _counter: number;
+export class LocationModel extends Observable {
   private _message: string;
-  private _loc: geolocation.Location;
-  private _lat: string;
+  private _lat: string = '';
+  private _lng: string = '';
+  private _alt: string = '';
 
   constructor() {
     super();
-    // Initialize default values.
-    geolocation.enableLocationRequest();
+    console.log('constructor');
     this.getLocation();
-    this._lat = 'Latitude: 0';
-  }
-
-  get message(): string {
-    return this._message;
-  }
-
-  set message(value: string) {
-    if (this._message !== value) {
-      this._message = value;
-      this.notifyPropertyChange('message', value);
-    }
   }
 
   get lat(): string {
-    if (this._lat !== 'undefined') return 'Latitude: ' + this._loc?.latitude;
+    if (this._lat !== 'undefined') return 'Latitude: ' + this._lat;
     else return 'Latitude: 0';
   }
 
   set lat(value: string) {
-    if (this._lat !== value) {
+    if (this._lat !== value && value !== 'undefined') {
       this._lat = value;
       this.notifyPropertyChange('lat', value);
-    } else this.lat = 'nothing new';
+    }
   }
 
   get lng(): string {
-    return this._loc?.longitude.toString();
+    if (this._lng !== 'undefined') return 'Lognitude: ' + this._lng;
+    else return 'Longitude: 0';
+  }
+
+  set lng(value: string) {
+    if (this._lng != value && value !== 'undefined') {
+      this._lng = value;
+      this.notifyPropertyChange('lng', value);
+    }
   }
 
   get alt(): string {
-    return this._loc?.altitude.toString();
+    if (this._alt !== 'undefined') return 'Elevation: ' + this._alt;
+    else return 'Elevation: 0';
+  }
+
+  set alt(value: string) {
+    if (this._alt != value && value !== 'undefined') {
+      this._alt = value;
+      this.notifyPropertyChange('alt', value);
+    }
   }
 
   onTap() {
     this.getLocation();
-    console.log(this._loc);
-
-    // Get current location with high accuracy
-  }
-
-  private updateMessage(ful) {
-    if (this._counter <= 0) {
-      this.message =
-        'Hoorraaay! You unlocked the NativeScript clicker achievement!';
-    } else {
-      this.message = ` lat:${ful?.latitude} lng:${ful?.longitude}`;
-    }
-
-    // log the message to the console
-    console.log(this.message);
-    console.log(ful);
   }
 
   private getLocation() {
-    var location = geolocation.getCurrentLocation({
-      desiredAccuracy: CoreTypes.Accuracy.high,
-      maximumAge: 5000,
-      timeout: 20000,
-    });
+    console.log('getLocation()');
+    var gl = geolocation
+      .isEnabled()
+      .then((isEnabled) => {
+        console.log(`isEnabled= ${isEnabled}`);
+        if (!isEnabled) {
+          console.log('send enabled location request');
+          gl = geolocation
+            .enableLocationRequest(true, true)
+            .then(() => {
+              console.log('enabled location request');
+              geolocation
+                .getCurrentLocation({
+                  desiredAccuracy: CoreTypes.Accuracy.high,
+                  maximumAge: 5000,
+                  timeout: 20000,
+                })
+                .then((loc) => {
+                  console.log(
+                    `getCurrentLocation inside enable loc ${JSON.stringify(
+                      loc
+                    )}`
+                  );
+                  this.setLatLongAlt(loc);
+                });
+            })
+            .catch((e) => {
+              console.log('catch geolocation.enableLocationRequest');
+              console.log(e);
+            });
+        } else {
+          console.log(`getCurrentLocation isEnabled = ${isEnabled}`);
+          geolocation
+            .getCurrentLocation({
+              desiredAccuracy: CoreTypes.Accuracy.high,
+              maximumAge: 5000,
+              timeout: 20000,
+            })
+            .then((loc) => {
+              console.log(`already enabled loc = ${JSON.stringify(loc)}`);
+              this.setLatLongAlt(loc);
+            })
+            .catch((error) => {
+              console.log(
+                `catch geolocation.getCurrentLocation error ${error}`
+              );
+            });
+        }
+      })
+      .catch((reason) => {
+        console.log(`catch geolocation.isEnabled reason = ${reason}`);
+      });
 
-    location.then((ful) => {
-      this._loc = ful;
-      this.lat = 'Latitude: ' + this._loc?.latitude;
-    });
+    console.log('exit getLocation');
+  }
+
+  // convert meters to feet
+  private m2f(meters: number): number {
+    return meters * 3.28084;
+  }
+
+  private setLatLongAlt(loc: geolocation.Location) {
+    const { latitude, longitude, altitude } = loc;
+    console.log(`lat = ${latitude}`);
+    let curLoc = JSON.parse(JSON.stringify(loc));
+    this.lat = 'Latitude: ' + latitude;
+    this.lng = 'Longitude: ' + longitude;
+    this.alt =
+      '' +
+      this.r(altitude, 1) +
+      'm/' +
+      this.r(this.m2f(altitude), 0).toString() +
+      'ft';
+  }
+  private r(n: number, places: number) {
+    let roundedNumber = Math.trunc(
+      (n * Math.pow(10, places)) / Math.pow(10, places)
+    );
+
+    return roundedNumber;
   }
 }
